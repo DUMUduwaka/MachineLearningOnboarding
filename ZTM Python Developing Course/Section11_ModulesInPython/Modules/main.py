4da93c5f6520ffca9d5e675d238a689706dc40a8
DUMUduwaka <mihirangausitha@gmail.com>
2025-06-26 15:43:54 +0530

    ZTM Python developer course

---
+++ ZTM Python Developing Course/Section11_ModulesInPython/Modules/main.py
@@ -0,0 +1,22 @@
+# from utility import multiply,divide
+# from shopping import shopping_cart
+#
+# if __name__ == '__main__':
+#     print(shopping_cart.buy)
+#     print(multiply(3.5,12))
+#     print(divide(2,3))
+#     print(shopping_cart.buy('car'))
+#     print(shopping_cart.buy('van'))
+#
+# print(__name__)
+
+import random
+
+print(random)
+print(random.random())
+print(random.randint(0,10))
+
+
+my_list = [1,2,3,4,5,6,7,8,9,10]
+random.shuffle(my_list)
+print(my_list)
